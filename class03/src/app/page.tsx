0bb23c888c64c8d8ee064f74df9d5a5f9c615017
Muhammad361738 <ullahehsan016@gmail.com>
2024-10-06 23:34:51 +0500

    some changes

--- class03/src/app/page.tsx
+++ class03/src/app/page.tsx
@@ -1,10 +1,10 @@
-// import Image from "next/image";
 
-import { PHASE_PRODUCTION_SERVER } from "next/dist/shared/lib/constants";
 import About from "./About/page";
 import Hero from "./Hero/hero";
 import Products from "./Products/page"
 import Navbar from "./Navbar/page";
+
+
 
 export default function Home() {
   return (
@@ -13,6 +13,7 @@ export default function Home() {
       {/* <About/> */}
       <Navbar/>
       <Hero/>
+  
       {/* <About/>
       <Products/> */}
      </div>
